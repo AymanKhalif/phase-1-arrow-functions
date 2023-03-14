c91de6abbd41c85883bfb10ee68370a69eb6893b
Aymankhalif <harithkhalif90@gmail.com>
2023-03-14 20:52:25 +0300

    done with the assignment

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = function () {
+    const divide = 2000 / 100 ;
+    return divide ;
+}
+const square = (parameter1) => parameter1 * parameter1 ;
+const add = (par1 , par2) => par1 + par2 ;
